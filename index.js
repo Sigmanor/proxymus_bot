f35274ffc02b32bcf8c57dccee21a6aaf2c55370
Alexander <sigmanor@pm.me>
2022-11-01 02:50:19 +0200

    test

--- index.js
+++ index.js
@@ -11,32 +11,14 @@ mongoose.connect(process.env.MONGO)
     .catch(function (error) {
         console.log(error);
     });
-    
-console.log(process.env.MONGO);   
 
 commands();
 
-(async function () {
-    const url = await ngrok.connect({
-        addr: 9997,
-        authtoken: process.env.NGROK,
-        region: 'eu',
+bot.launch({dropPendingUpdates: true})
+    .then(() => console.log(`${new Date()} It\'s Alive!`))
+    .catch(function (error) {
+        console.log(error);
     });
-    console.log(`tunnel: ${url.magenta}`);
-    await bot.launch({
-        dropPendingUpdates: true,
-        webhook: {
-            domain: url,
-            port: 9997,
-        },
-    });
-    console.log(
-        `\n${dateFormat(new Date(), '[dd.mm.yyyy HH:MM:ss]').blue
-        } Bot successfully started ^_^\n`
-    );
-})();
 
 process.once('SIGINT', () => bot.stop('SIGINT'));
 process.once('SIGTERM', () => bot.stop('SIGTERM'));
-
-//test
